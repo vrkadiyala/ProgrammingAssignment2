8d62887a0cbc793c731bf43a7834361988db1b95
vrkadiyala <kadiyala_2000@yahoo.com>
2014-07-27 23:57:44 +0530

    implementation of lexical scope and caching function
    updated the script with the implementation of cache inverse matrix by
    applying lexical scope concepts.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_x <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    inv_x <<- NULL
+  }
+  get <- function()x
+  setinverse <- function(solve) inv_x <<- solve
+  getinverse <- function() inv_x
+  list( set = set,
+        get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
 
@@ -12,4 +24,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if ( !is.null(i))
+  {
+    message(" getting cached inverse matrix")
+    return(i)
+  }
+  else
+  {
+    dat <- x$get()
+    i <- solve(dat, ...)
+    x$setinverse(i)
+    i
+  }
 }
